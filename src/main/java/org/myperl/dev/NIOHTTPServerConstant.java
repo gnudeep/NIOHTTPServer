e8662effde80abef1a337be059fc41f3aebfb66c
Deependra Ariyadewa <deependra@gmail.com>
2013-07-14 18:50:02 +0000

    custom webroot suppot and maven assembly plugin added.

---
+++ src/main/java/org/myperl/dev/NIOHTTPServerConstant.java
@@ -0,0 +1,11 @@
+package org.myperl.dev;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: deep
+ * Date: 7/14/13
+ * Time: 5:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NIOHTTPServerConstant {
+}
